d5988516c77d081805b4f921148a52a81530c1fa
Dat Tran Tien <tiendat0274@gmail.com>
2022-06-26 00:31:01 +0700

    Updates

---
+++ common/build/events/types/ticket-status.d.ts
@@ -0,0 +1,5 @@
+export declare enum TicketStatus {
+    Available = "available",
+    NotAvailable = "not-available",
+    OutOfStock = "out-of-stock"
+}
---
+++ common/build/events/types/ticket-status.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TicketStatus = void 0;
+var TicketStatus;
+(function (TicketStatus) {
+    TicketStatus["Available"] = "available";
+    TicketStatus["NotAvailable"] = "not-available";
+    TicketStatus["OutOfStock"] = "out-of-stock";
+})(TicketStatus = exports.TicketStatus || (exports.TicketStatus = {}));
